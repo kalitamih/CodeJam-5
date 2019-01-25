df0d01b8049917956d5520a108412d225a054e5e
zhechka <aabigor89@gmail.com>
2019-01-25 03:41:33 +0200

    feat: deploy gh-pages

---
+++ culture-portal/precache-manifest.712ce7da8169c68eb8a8c45e14f4d280.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12ce54fe26d27ea4f288e6ce5cc5d584",
+    "url": "/CodeJam-5/static/media/Ryhor_Baradulin.12ce54fe.jpg"
+  },
+  {
+    "revision": "cd8001780f013eb8d0fb",
+    "url": "/CodeJam-5/static/js/runtime~main.cd800178.js"
+  },
+  {
+    "revision": "283cd91b496156662d70",
+    "url": "/CodeJam-5/static/js/main.283cd91b.chunk.js"
+  },
+  {
+    "revision": "1c8afda889f30a64c9d9",
+    "url": "/CodeJam-5/static/js/1.1c8afda8.chunk.js"
+  },
+  {
+    "revision": "283cd91b496156662d70",
+    "url": "/CodeJam-5/static/css/main.3f20c353.chunk.css"
+  },
+  {
+    "revision": "1c8afda889f30a64c9d9",
+    "url": "/CodeJam-5/static/css/1.84d425be.chunk.css"
+  },
+  {
+    "revision": "1cd0c7a23832f49fa82a9ca6b6dfadfb",
+    "url": "/CodeJam-5/index.html"
+  }
+];
---
+++ culture-portal/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/CodeJam-5/precache-manifest.712ce7da8169c68eb8a8c45e14f4d280.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/CodeJam-5/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
