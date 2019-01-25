bd2f2baf43b740ed938b986fa1dfa20add44c133
zhechka <aabigor89@gmail.com>
2019-01-25 03:26:57 +0200

    Updates

---
+++ culture-portal/src/components/Navigation.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Button
+} from "reactstrap";
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Culture Portal</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Button color="link" onClick={this.props.ChangePage}>
+                  Writers
+                </Button>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="#about_us">Collaborators</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
---
+++ culture-portal/src/components/Writers.jsx
@@ -0,0 +1,123 @@
+import React, { Component } from "react";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
+import Avatar from "./img/Ryhor_Baradulin.jpg";
+import Navigation from "./Navigation";
+
+class Writers extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="writers">
+        <Navigation />
+        <div className="search_container">
+          <Container>
+            <Row>
+              <Col>
+                <Form>
+                  <FormGroup>
+                    <Label for="exampleSearch">Search Author</Label>
+                    <Input
+                      type="search"
+                      name="search"
+                      id="exampleSearch"
+                      placeholder="search placeholder"
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="exampleSelect">Filter by City</Label>
+                    <Input
+                      type="select"
+                      name="Filter by City"
+                      id="exampleSelect"
+                    >
+                      <option>Minsk</option>
+                      <option>Vitebsk</option>
+                      <option>Grodno</option>
+                      <option>Babruisk</option>
+                      <option>Magilev</option>
+                    </Input>
+                  </FormGroup>
+                </Form>
+              </Col>
+              <Col>
+                <Card>
+                  <CardImg top src={Avatar} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Григорий Бородулин</CardTitle>
+                    <CardSubtitle>Витебская обл</CardSubtitle>
+                    <CardText>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </CardText>
+                    <Button onClick={this.props.GoToWriterPage}>
+                      Read more
+                    </Button>
+                  </CardBody>
+                </Card>
+                <Card>
+                  <CardImg top src={Avatar} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Григорий Бородулин</CardTitle>
+                    <CardSubtitle>Витебская обл</CardSubtitle>
+                    <CardText>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </CardText>
+                    <Button onClick={this.props.GoToWriterPage}>
+                      Read more
+                    </Button>
+                  </CardBody>
+                </Card>
+                <Card>
+                  <CardImg top src={Avatar} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Григорий Бородулин</CardTitle>
+                    <CardSubtitle>Витебская обл</CardSubtitle>
+                    <CardText>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </CardText>
+                    <Button onClick={this.props.GoToWriterPage}>
+                      Read more
+                    </Button>
+                  </CardBody>
+                </Card>
+                <Card>
+                  <CardImg top src={Avatar} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Григорий Бородулин</CardTitle>
+                    <CardSubtitle>Витебская обл</CardSubtitle>
+                    <CardText>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </CardText>
+                    <Button onClick={this.props.GoToWriterPage}>
+                      Read more
+                    </Button>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Writers;
